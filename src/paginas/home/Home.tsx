eb0e8a2d86dc1d6f910f788af7a0f2b0820be6b8
ElissandroAbreu <elissandro.abreus13@outlook.com>
2023-04-10 19:44:49 -0300

    Componentes Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return (
+        <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVuf
+        DB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Imagem Tela Inicial" className="img" />
+        </>
+    );
+}
+
+export default Home;
