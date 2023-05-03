122f8c0dd09fc98fe5ae613a1ccfb2f0b4938568
ElissandroAbreu <elissandro.abreus13@outlook.com>
2023-05-03 14:14:53 -0300

    Ajustes

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -22,16 +22,11 @@ function Footer() {
             <Grid alignItems="center" item xs={12}>
                 <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
                     <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                        <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                        <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Meu LinkedIn </Typography>
                     </Box>
                     <Box display="flex" alignItems="center" justifyContent="center">
-                        <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
-                            <FacebookIcon style={{ fontSize: 60, color: "white" }} />
-                        </a>
-                        <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                            <InstagramIcon style={{ fontSize: 60, color: "white" }} />
-                        </a>
-                        <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
+                        
+                        <a href="https://www.linkedin.com/in/elissandro-abreu/" target="_blank" rel="noopener noreferrer">
                             <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
                         </a>
                     </Box>
@@ -41,8 +36,8 @@ function Footer() {
                         <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2023 Copyright:</Typography>
                     </Box>
                     <Box>
-                        <a target="_blank" href="https://brasil.generation.org" rel="noopener noreferrer">
-                            <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
+                        <a target="_blank" href="https://github.com/ElissandroAbreu" rel="noopener noreferrer">
+                            <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">Elissandro de Abreu</Typography>
                         </a>
                     </Box>
                 </Box>
--- src/components/postagens/tabpostagens/TabPostagem.tsx
+++ src/components/postagens/tabpostagens/TabPostagem.tsx
@@ -17,7 +17,7 @@ function TabPostagem() {
                 <AppBar position="static">
                     <Tabs centered indicatorColor="secondary" onChange={handleChange}>
                         <Tab label="Todas as postagens" value="1" />
-                        <Tab label="Sobre-nós" value="2" />
+                        <Tab label="Sobre" value="2" />
                     </Tabs>
                 </AppBar>
                 <TabPanel value="1" >
@@ -26,8 +26,8 @@ function TabPostagem() {
                     </Box>
                 </TabPanel>
                 <TabPanel value="2">
-                    <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center">Sobre-nós</Typography>
-                    <Typography variant="body1" gutterBottom color="textPrimary" align="justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos ut eveniet natus totam et, voluptate dicta tempore alias, odio nobis non eius cupiditate minima inventore pariatur! Ipsum itaque consectetur voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo velit consequuntur suscipit fugiat, nam quis quod quaerat veritatis et, vel ratione beatae, facere neque! Quo animi porro voluptate saepe deleniti? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore adipisci, officia aut quidem dolorum deserunt iure dolorem doloribus velit nobis quas consequatur at ullam odit, nesciunt est nulla nihil excepturi!</Typography>
+                    <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center">O blog e seus objetivos</Typography>
+                    <Typography variant="body1" gutterBottom color="textPrimary" align="justify">O blog tem o intuito de expressar suas ideias e opiniões</Typography>
                 </TabPanel>
             </TabContext>
         </>
